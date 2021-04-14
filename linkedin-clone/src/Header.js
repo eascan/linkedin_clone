36915ae6c2900c0569c9b85bfedca872e13e9be4
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-14 15:44:39 -0400

    install material ui and icons

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -5,6 +5,17 @@ function Header() {
   return (
     <div className="header">
       <h1>THIS is the header</h1>
+
+      <div className="header__left">
+        <img src="" alt="" />
+
+        <div className="header__search">
+          {/* {searchicon} */}
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right"></div>
     </div>
   );
 }
