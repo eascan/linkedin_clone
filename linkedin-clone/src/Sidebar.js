d74665ad655f496cba4e90d661679229d7287471
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-18 16:58:51 -0400

    add bottom left side bar

--- linkedin-clone/src/Sidebar.js
+++ linkedin-clone/src/Sidebar.js
@@ -1,10 +1,48 @@
+import {Avatar} from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => {
+    return (
+      <div className="sidebar__recentItem">
+        <span className="sidebar__hash">#</span>
+        <p>{topic}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="sidebar">
-      <h3>I'm sidebar</h3>
+      <div className="sidebar__top">
+        <img
+          src="https://www.geeklawblog.com/wp-content/uploads/sites/528/2018/12/liprofile-656x369.png "
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Edward Ascan</h2>
+        <h4>eascan.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2300</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
     </div>
   );
 }
