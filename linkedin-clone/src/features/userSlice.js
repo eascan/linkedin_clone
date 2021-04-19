4433162e292b42aab16ec4ad4537e1bc7011b883
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-04-19 15:21:37 -0400

    cleanup userslice

--- linkedin-clone/src/features/userSlice.js
+++ linkedin-clone/src/features/userSlice.js
@@ -1,5 +1,5 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {fetchCount} from "./counterAPI";
+// import {fetchCount} from "./counterAPI";
 
 const initialState = {
   value: 0,
@@ -13,39 +13,36 @@ export const userSlice = createSlice({
   },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    login: (state, action) => {
+      state.value = action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    logout: (state) => {
+      state.user = null;
     },
   },
 
-  extraReducers: (builder) => {
-    builder
-      .addCase(incrementAsync.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
-        state.status = "idle";
-        state.value += action.payload;
-      });
-  },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(incrementAsync.pending, (state) => {
+  //       state.status = "loading";
+  //     })
+  //     .addCase(incrementAsync.fulfilled, (state, action) => {
+  //       state.status = "idle";
+  //       state.value += action.payload;
+  //     });
+  // },
 });
 
-export const {increment, decrement, incrementByAmount} = userSlice.actions;
+export const {login, logout} = userSlice.actions;
 
+// Selectors
 export const selectUser = (state) => state.user.user;
 
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
+// export const incrementIfOdd = (amount) => (dispatch, getState) => {
+//   const currentValue = selectCount(getState());
+//   if (currentValue % 2 === 1) {
+//     dispatch(incrementByAmount(amount));
+//   }
+// };
 
 export default userSlice.reducer;
