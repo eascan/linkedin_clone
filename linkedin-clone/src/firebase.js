5760072c5f3d1052bc2ebb0676ec788255d2a652
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-19 11:07:20 -0400

    add onclick to form button in feed, installed firebase and firebase config to project

---
+++ linkedin-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjotLNlMVpjpDgkiY_XcS__gofx_8KFm0",
+  authDomain: "linkedin-clone-13588.firebaseapp.com",
+  projectId: "linkedin-clone-13588",
+  storageBucket: "linkedin-clone-13588.appspot.com",
+  messagingSenderId: "846717811010",
+  appId: "1:846717811010:web:59ce853e6e93c272f230db",
+};
