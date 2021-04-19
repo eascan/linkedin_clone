9c844ef0ee090a1059f25b6b26af48bfd07b559d
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-18 20:18:50 -0400

    more styling on feed/post, add state to feed

--- linkedin-clone/src/Feed.js
+++ linkedin-clone/src/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
@@ -9,6 +9,8 @@ import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -31,6 +33,11 @@ function Feed() {
         </div>
       </div>
 
+      {/* posts */}
+
+      {posts.map((post) => {
+        <Post />;
+      })}
       <Post
         name="Edward Ascan"
         description="this is test"
