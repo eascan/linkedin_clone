f1f312833ca98d460ff4caa2593c0b44a9ec8ada
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-20 19:31:52 -0400

    add widgets section and some styling

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -8,6 +8,7 @@ import {login, logout, selectUser} from "./features/userSlice";
 import Login from "./Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "./firebase";
+import Widgets from "./Widgets";
 
 function App() {
   const user = useSelector(selectUser);
@@ -40,7 +41,7 @@ function App() {
         <div className="app__body">
           <Sidebar />
           <Feed />
-          {/* {Widgets} */}
+          <Widgets />
         </div>
       )}
     </div>
---
+++ linkedin-clone/src/Widgets.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+
+      {newsArticle("Top news", "9099 readers")}
+      {newsArticle("Tesla stocks hit new highs", "801 readers")}
+      {newsArticle("Coronavirus: Canada updates", "2324 readers")}
+      {newsArticle("Bitcoin - yay or nay", "999 readers")}
+    </div>
+  );
+}
+
+export default Widgets;
